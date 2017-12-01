7749ee472ab37f4935075752a9bdc44fb2cde6ea
lemmi <lemmi@nerd2nerd.org>
2017-12-02 00:48:01 +0100

    adapt gitignore to multproject folder

--- star01/target/debug/star01.d
+++
@@ -1,1 +0,0 @@
-/home/lemmi/rust/AOC-2017/star01/target/debug/star01: /home/lemmi/rust/AOC-2017/star01/src/main.rs
--- star02/target/debug/star02.d
+++
@@ -1,1 +0,0 @@
-/home/lemmi/rust/AOC-2017/star02/target/debug/star02: /home/lemmi/rust/AOC-2017/star02/src/main.rs
