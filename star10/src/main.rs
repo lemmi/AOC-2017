e5cd860f8aee79a28651d56a6ab95b662b71c3e9
lemmi <lemmi@nerd2nerd.org>
2017-12-05 12:25:56 +0100

    Solutions for day 05

---
+++ star10/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let mut pos = 0isize;
+    let mut count = 0isize;
+    let stdin = io::stdin();
+    let mut table: Vec<isize> = stdin.lock()
+        .lines()
+        .map(
+            |s| s.expect("Oo?")
+                .parse()
+                .expect("Not a number!")
+            )
+        .collect();
+
+    while 0 <= pos && pos < table.len() as isize{
+        let t = table[pos as usize];
+        if t >= 3 {
+            table[pos as usize] -= 1;
+        } else {
+            table[pos as usize] += 1;
+        }
+        pos += t;
+        count += 1;
+    }
+
+    println!("{} steps taken", count);
+}
