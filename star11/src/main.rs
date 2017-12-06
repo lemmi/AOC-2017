8a2d7121436336dfb376eec0f3c2bc4acbef8b82
lemmi <lemmi@nerd2nerd.org>
2017-12-06 08:22:26 +0100

    Solutions for day 06

---
+++ star11/src/main.rs
@@ -0,0 +1,81 @@
+use std::io;
+use std::io::BufRead;
+use std::collections::BTreeSet;
+
+#[derive(Copy,Clone,PartialEq,Eq,Debug,PartialOrd,Ord,Default)]
+struct State {
+    banks: [u8; 16],
+}
+
+impl From<Vec<u8>> for State {
+    fn from(s: Vec<u8>) -> State {
+        let mut ret = State::default();
+        for (from, to) in s.iter().zip(ret.banks.iter_mut()) {
+            *to = *from;
+        }
+        ret
+    }
+}
+
+impl State {
+    fn find_max(&self) -> usize {
+        let mut max = self.banks[0];
+        let mut max_index = 0usize;
+
+        for (i, bank) in self.banks.iter().enumerate() {
+            if *bank > max {
+                max = *bank;
+                max_index = i;
+            }
+        }
+
+        max_index
+    }
+}
+
+impl Iterator for State {
+    type Item = State;
+
+    fn next(&mut self) -> Option<State> {
+        let mut idx = self.find_max();
+        let mut n = self.banks[idx];
+
+        self.banks[idx] = 0;
+        while n > 0 {
+            idx = (idx + 1) % self.banks.len();
+            self.banks[idx] += 1;
+            n -= 1;
+        }
+
+        Some(self.clone())
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let start: Vec<u8> = stdin.lock()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .split_whitespace()
+        .map(
+            |s| s.parse()
+                .expect("Not a number!")
+            )
+        .collect();
+
+    let start = State::from(start);
+
+    let mut states = BTreeSet::new();
+    states.insert(start);
+
+    for state in start {
+        println!("{:?}", state);
+        if !states.insert(state) {
+            break;
+        }
+    }
+    println!("Found duplicate! {}", states.len());
+
+}
